9a342b4a2c0e4411081ea1e94370510c00efcfcd
Abralava Georgii <69796158+gabralava@users.noreply.github.com>
2024-06-07 21:56:16 +0300

    Update UserServiceLoggingAspect.java

--- demo/src/main/java/com/example/demo/aop/UserServiceLoggingAspect.java
+++ demo/src/main/java/com/example/demo/aop/UserServiceLoggingAspect.java
@@ -1,23 +1,23 @@
-// package com.example.demo.aop;
+package com.example.demo.aop;
 
-// import lombok.extern.slf4j.Slf4j;
-// import org.aspectj.lang.JoinPoint;
-// import org.aspectj.lang.annotation.AfterReturning;
-// import org.aspectj.lang.annotation.Aspect;
-// import org.aspectj.lang.annotation.Pointcut;
-// import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
-// @Aspect
-// @Component
-// @Slf4j
-// public class UserServiceLoggingAspect {
+@Aspect
+@Component
+@Slf4j
+public class UserServiceLoggingAspect {
 
-//     @Pointcut("execution(* com.example.demo.service.UserService.*(..))")
-//     public void userServiceMethods() {}
+    @Pointcut("execution(* com.example.demo.service.UserService.*(..))")
+    public void userServiceMethods() {}
 
-//     @AfterReturning(value = "userServiceMethods()", returning = "result")
-//     public void logUserServiceMethods(JoinPoint joinPoint, Object result) {
-//         String methodName = joinPoint.getSignature().getName();
-//         log.info("Method {} from class UserService is called", methodName);
-//     }
-// }
+    @AfterReturning(value = "userServiceMethods()", returning = "result")
+    public void logUserServiceMethods(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Method {} from class UserService is called", methodName);
+    }
+}
