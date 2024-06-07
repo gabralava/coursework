02019a57fa6b66ad8fb1e956e46494aae423d973
Abralava Georgii <69796158+gabralava@users.noreply.github.com>
2024-06-07 21:58:03 +0300

    Update PasswordControllerLoggingAspect.java

--- demo/src/main/java/com/example/demo/aop/PasswordControllerLoggingAspect.java
+++ demo/src/main/java/com/example/demo/aop/PasswordControllerLoggingAspect.java
@@ -1,22 +1,22 @@
-// package com.example.demo.aop;
+package com.example.demo.aop;
 
-// import org.aspectj.lang.JoinPoint;
-// import org.aspectj.lang.annotation.Aspect;
-// import org.aspectj.lang.annotation.Before;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.stereotype.Component;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-// @Aspect
-// @Component
-// public class PasswordControllerLoggingAspect {
+@Aspect
+@Component
+public class PasswordControllerLoggingAspect {
 
-//     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-//     @Before("execution(* com.example.demo.controller.PasswordController.*(..))")
-//     public void logPasswordControllerMethodCalls(JoinPoint joinPoint) {
-//         String methodName = joinPoint.getSignature().getName();
-//         String className = joinPoint.getTarget().getClass().getSimpleName();
-//         logger.info("Method {} from class {} is called", methodName, className);
-//     }
-// }
+    @Before("execution(* com.example.demo.controller.PasswordController.*(..))")
+    public void logPasswordControllerMethodCalls(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        logger.info("Method {} from class {} is called", methodName, className);
+    }
+}
