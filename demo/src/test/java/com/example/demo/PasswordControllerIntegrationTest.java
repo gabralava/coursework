3c8fbe6d0ae5efa18302345385ecd5a17d909dfe
Abralava Georgii <georgijabralava@gmail.com>
2024-06-09 13:30:49 +0300

    commit

--- demo/src/test/java/com/example/demo/PasswordControllerIntegrationTest.java
+++ demo/src/test/java/com/example/demo/PasswordControllerIntegrationTest.java
@@ -32,7 +32,7 @@ public class PasswordControllerIntegrationTest {
     @WithMockUser(username = "newuser", password = "newpassword")
     public void testGeneratePassword() throws Exception {
         mockMvc.perform(post("/passwords/generate?length=12&siteUrl=http://example.com"))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.password").isNotEmpty());
     }
 
@@ -40,7 +40,7 @@ public class PasswordControllerIntegrationTest {
     @WithMockUser(username = "newuser", password = "newpassword")
     public void testGetUserPasswords() throws Exception {
         mockMvc.perform(get("/passwords"))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
